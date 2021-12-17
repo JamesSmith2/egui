87312ce29122f4b43844b25deef869dd148e11d3
Jaanus <jlepik442@gmail.com>
2021-12-17 11:09:08 +0200

    disable vsync

--- egui_glow/src/epi_backend.rs
+++ egui_glow/src/epi_backend.rs
@@ -21,9 +21,9 @@ fn create_display(
     let gl_window = unsafe {
         glutin::ContextBuilder::new()
             .with_depth_buffer(0)
-            .with_srgb(true)
+            .with_srgb(false)
             .with_stencil_buffer(0)
-            .with_vsync(true)
+            .with_vsync(false)
             .build_windowed(window_builder, event_loop)
             .unwrap()
             .make_current()
